90469a3295d3d4518bf65954a1c54ba824bec64d
unknown <csanchis13@gmail.coms>
2025-01-10 10:33:30 +0100

    Anyadido IOUs en evaluacion

--- evaluate/evaluate.py
+++ evaluate/evaluate.py
@@ -4,6 +4,7 @@ from pathlib import Path
 from argparse import ArgumentParser
 import argparse
 import os
+import numpy as np
 
 import yaml
 import json
@@ -44,7 +45,7 @@ import torch.nn.functional as F
 from torch.utils.data import DataLoader
 from torchvision import transforms
 from pathlib import Path
-from metricas import calculate_auc_multilabel
+from metricas import calculate_auc_multilabel, iou
 from pl_constrained_model import ConstrainedSegmentMIL
 import m_dataLoad_json
 
@@ -98,6 +99,7 @@ if __name__ == "__main__":
     maxvalues=config['data']['maxvalues']
     crop_size=config['data']['crop_size']
     tipos_defecto=config['model']['defect_types']
+    use_masks=config['data']['use_segmentation_masks']
     
     
     model_dir=config['evaluate']['model_dir']
@@ -137,21 +139,26 @@ if __name__ == "__main__":
     print('root_folder:',root_folder)
     print('train_dataplaces num_directories',len(train_dataplaces)) 
     dataset,tipos_defecto=m_dataLoad_json.genera_ds_jsons_multilabel(root_folder,  train_dataplaces, sufijos=terminaciones,maxValues=maxvalues, crop_size=crop_size,
-                                                                            defect_types=tipos_defecto,splitname_delimiter=delimiter,multilabel=True, in_memory=False)
+                                                                            defect_types=tipos_defecto,splitname_delimiter=delimiter,multilabel=True, in_memory=True,use_masks=use_masks)
     class_results=[]
-
+    bin_masks=[]
+    score_maps_list=[]
 
     for caso in tqdm(dataset):
         targets=caso['labels']
         fruit_id=caso['fruit_id']
         view_id=caso['view_id']
         imags_folder=caso['imag_folder']
+        bin_mask=caso['bin_masks']
         view_id=os.path.join(imags_folder,view_id)
         results=model.predict(view_id,device,include_images=False,remove_suffix=False)
         result= results[0]
        
         preds_train.append(result['view_probs_tensor'])
+        score_maps=result['pixel_probs_tensor']
+        score_maps_list.append(score_maps.squeeze(0))
         targets_train.append(targets.unsqueeze(dim=0))
+        bin_masks.append(bin_mask)
         targets_dict={}
         
         for nclase in range(len(tipos_defecto)):
@@ -176,10 +183,45 @@ if __name__ == "__main__":
 
     preds_train = torch.stack(preds_train)
     targets_train = torch.concat(targets_train)
-    aucs_train=calculate_auc_multilabel(preds_train,targets_train,tipos_defecto)   
+
+    print('preds_train.shape',preds_train.shape)
+    print('targets_train.shape',targets_train.shape)
+    print('bin_masks.shape',len(bin_masks))
+    aucs_train=calculate_auc_multilabel(preds_train,targets_train,tipos_defecto)
+    threshold=0.5
+    ious_train=[]
+    ious_neutros=[]
+    for i in range(len(tipos_defecto)):
+        ious_train.append([])
+        ious_neutros.append([])
+    for b in range(len(bin_masks)):
+        for c in range(len(tipos_defecto)):
+            missing_mask=True
+            if bin_masks[b] is not None:
+                if bin_masks[b][c] is not None:
+                    iou_t=iou(bin_masks[b][c],score_maps_list[b][c],threshold=threshold)
+                    ious_train[c].append(iou_t)
+                    missing_mask=False
+            if missing_mask:
+                if targets_train[b][c] == 0:
+                    if preds_train[b][c] > threshold:
+                        ious_train[c].append(0.0)
+                    else:
+                        # ious_train[c].append(1.0)
+                        ious_neutros[c].append(1.0)
+                else:
+                    if preds_train[b][c] < threshold:
+                        ious_train[c].append(0.0)   
+    ious_train_dict={}
+    for c in range(len(tipos_defecto)):
+        ious_neutros[c]+=ious_train[c]
+        iou1=np.mean(np.array(ious_train[c]))
+        iou2=np.mean(np.array(ious_neutros[c]))
+        ious_train_dict[tipos_defecto[c]]=(iou1,iou2)
 
     preds_val=[]
     targets_val=[]
+    bin_masks=[]
     #ids_train=[]
     json_data=[]
     print("\n\n====================================================================")
@@ -188,14 +230,16 @@ if __name__ == "__main__":
     print('root_folder:',root_folder)
     print('val_dataplaces num_directories',len(val_dataplaces)) 
     dataset,tipos_defecto=m_dataLoad_json.genera_ds_jsons_multilabel(root_folder,  val_dataplaces, sufijos=terminaciones,maxValues=maxvalues, crop_size=crop_size,
-                                                                            defect_types=tipos_defecto,splitname_delimiter=delimiter,multilabel=True, in_memory=False)
+                                                                            defect_types=tipos_defecto,splitname_delimiter=delimiter,multilabel=True, in_memory=True,use_masks=use_masks)
     class_results=[]
+    score_maps_list_val=[]
     for caso in tqdm(dataset):
         #image=caso['image']
         targets=caso['labels']
         fruit_id=caso['fruit_id']
         view_id=caso['view_id']
         imags_folder=caso['imag_folder']
+        bin_mask=caso['bin_masks']
         view_id=os.path.join(imags_folder,view_id)
         results=model.predict(view_id,device,include_images=False,remove_suffix=False)
         result= results[0]
@@ -203,7 +247,10 @@ if __name__ == "__main__":
         #results=model.predict(image,device)
         #print(result['view_probs_tensor'])
         preds_val.append(result['view_probs_tensor'])
+        score_maps=result['pixel_probs_tensor']
+        score_maps_list_val.append(score_maps.squeeze(0))
         targets_val.append(targets.unsqueeze(dim=0))
+        bin_masks.append(bin_mask)
         targets_dict={}
         
         for nclase in range(len(tipos_defecto)):
@@ -240,9 +287,54 @@ if __name__ == "__main__":
     targets_val = torch.concat(targets_val)    
     aucs_val=calculate_auc_multilabel(preds_val,targets_val,tipos_defecto)
 
+    print("\n====================================================================")
+    print("  ******************** IOUs *************************")
+    print("====================================================================")
+
+    threshold=0.5
+    ious_val=[]
+    ious_neutros=[]
+    for i in range(len(tipos_defecto)):
+        ious_val.append([])
+        ious_neutros.append([])
     
+    for b in range(len(bin_masks)):
+        for c in range(len(tipos_defecto)):
+            missing_mask=True
+            if bin_masks[b] is not None:
+                if bin_masks[b][c] is not None:
+                    iou1=iou(bin_masks[b][c],score_maps_list_val[b][c],threshold=threshold)
+                    ious_val[c].append(iou1)
+                   
+                    missing_mask=False
+            if missing_mask:
+                if targets_val[b][c] == 0:
+                    if preds_val[b][c] > threshold:
+                        ious_val[c].append(0.0)
+                    
+                    else:
+                        # ious_val[c].append(1.0)
+                        ious_neutros[c].append(1.0)
+                else:
+                    if preds_val[b][c] < threshold:
+                        ious_val[c].append(0.0)
+                       
+
+    ious_val_dict={}
+   
+ 
+    for c in range(len(tipos_defecto)):
+        ious_neutros[c]+=ious_val[c]
+        iou1=np.mean(np.array(ious_val[c]))
+        iou2=np.mean(np.array(ious_neutros[c]))
+        ious_val_dict[tipos_defecto[c]]=(iou1,iou2)
+    
+    
+   
     aucdata={'Train AUCs':aucs_train,
              'Val AUCs': aucs_val,
+             'Train IOUs': ious_train_dict,
+             'Val IOUs': ious_val_dict,
              'evaluation_date': str(datetime.now()),
              'training_date':training_date,
              'model_file':model_path}
